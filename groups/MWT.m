df536d7a1a21c069c6e2097dafb992e3167572dc
katdeane <katdeane92@gmail.com>
2023-07-05 15:23:05 -0700

    update animal group metadata

--- groups/MWT.m
+++ groups/MWT.m
@@ -1,27 +1,28 @@
 %%% Group MWT = Male Wild Type (C57 B6/J)
-animals = {'MWT01','MWT02','MWT03'};
+animals = {'MWT01','MWT02','MWT03','MWT04'};
 
 % ruled out:
-% CIC03 - had one successful recording day and then nothing but noise on day 2 and 3. Setup was confirmed ok. Hemorrhaging suspected but could not be confirmed
-% CIC05 - no recording beyond mapping during surgery, animal died in recovery
-% CIC01 & CIC04 - no redeemable signal after implantation 
+% 
 
 %% Channels and Layers
 
 channels = {...
-    '[1 32]',... %01
-    '[1 32]',... %02
-    '[1 32]',... %03
+    '[5 30]',... %01
+    '[5 29]',... %02
+    '[4 30]',... %03
+    '[5 29]',... %04
     };
 
-%           01          02          03           
-Layer.II = {'[1:3]',	'[1:3]',    '[1:3]'}; 
-%           01          02          03
-Layer.IV = {'[4:10]',	'[4:9]',    '[4:9]'};
-%           01          02          03
-Layer.V  = {'[11:14]',	'[10:15]',  '[10:15]'};
-%           01          02          03
-Layer.VI = {'[15:20]',	'[16:20]',  '[16:20]'}; 
+%           01          02          03          04 
+Layer.II = {'[1:3]',	'[1:3]',    '[1:3]',    '[1:3]'}; 
+%           01          02          03          04
+Layer.IV = {'[4:10]',	'[4:10]',   '[4:10]',   '[4:9]'};
+%           01          02          03          04
+Layer.Va = {'[11:15]',	'[11:15]',  '[11:16]',  '[10:16]'};
+%           01          02          03          04
+Layer.Vb = {'[16:22]',	'[16:21]',  '[17:23]',  '[17:23]'};
+%           01          02          03          04
+Layer.VI = {'[23:26]',	'[22:25]',  '[24:27]',  '[24:25]'}; 
 
 
 
@@ -30,46 +31,54 @@ Cond.NoiseBurst = {...
 	{'01'},... %MWT01 % software wouldn't connect to server, electrode was in for 40 minutes before I started
 	{'01','02','03','04','05'},... %MWT02
     {'01','02','03','04','05','06'},... %MWT03
+    {'01','02','03','04','05','06'},... %MWT04
     };
 
 Cond.Tonotopy = {...
 	{'02'},... %MWT01
     {'06'},... %MWT02
     {'07'},... %MWT03
+    {'07'},... %MWT04
     };
 
 Cond.Spontaneous = {...
 	{'03'},... %MWT01
     {'07'},... %MWT02
     {'08'},... %MWT03
+    {'08'},... %MWT04
 	};
 
 Cond.ClickTrain = {...
 	{'04'},... %MWT01
     {'09','14'},... %MWT02
     {'11','16'},... %MWT03
+    {'09'},... %MWT04
 	};
 
 Cond.Chirp = {...
 	{'05'},... %MWT01
     {'08','13'},... %MWT02
     {'10','15'},... %MWT03
+    {'11'},... %MWT04
     };
 
 Cond.gapASSR = {...
 	{'06'},... %MWT01
     {'10','15'},... %MWT02
     {'09','14'},... %MWT03
+    {'10'},... %MWT04
     };
 
 Cond.postNoise = {...
 	{'07'},... %MWT01
     {'11','16'},... %MWT02
     {'12','17'},... %MWT03
+    {'12'},... %MWT04
     };
 
 Cond.postSpont = {...
 	{'08'},... %MWT01
     {'12','17'},... %MWT02
     {'13','18'},... %MWT03
+    {'13'},... %MWT04
     };
