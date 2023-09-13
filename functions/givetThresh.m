63f056fdb368999a0c05746fe0d321f86cfe05c3
katdeane <katdeane92@gmail.com>
2023-09-13 15:43:12 +0000

    created function to provide one or two tailed t threshold for alpha 0.05 with group size input

---
+++ functions/givetThresh.m
@@ -0,0 +1,14 @@
+function [t_two_tailed, t_one_tailed] = givetThresh(group_size1, group_size2)
+% written by chatGPT, credit where it's due. Thanks bud
+    % Calculate the degrees of freedom
+    df = group_size1 + group_size2 - 2;
+    
+    % Set the significance level (alpha)
+    alpha = 0.05;
+    
+    % Calculate the t-critical value for a two-tailed test
+    t_two_tailed = tinv(1 - alpha / 2, df);
+    
+    % Calculate the t-critical value for a one-tailed test (positive direction)
+    t_one_tailed = tinv(1 - alpha, df);
+end
