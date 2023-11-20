ac9b2105951bba6eff2dfd83dee3f7ceff7c6fa3
katdeane <katdeane92@gmail.com>
2023-11-20 19:41:45 +0000

    remove downsampling, data downsampled by 30 out of curate

--- functions/FileReaderLFP.m
+++ functions/FileReaderLFP.m
@@ -1,20 +1,24 @@
 function [stimIn, data] = FileReaderLFP(file,channels)
-% This converts the data from allego/curate and downsamples it by 30. That
-% results in sr = 1000 (1000 sp in 1 second / each sp is 1 ms)
+% This converts the data from allego/curate, assuming it has already been 
+% downsampled by 30. 
+% sr = 1000 (1000 sp in 1 second / each sp is 1 ms)
+
 % initalized NeuroNexus conversion function
 reader = allegoXDatFileReaderR2019b;
 
 timerange = reader.getAllegoXDatTimeRange(file);
 signalStruct = reader.getAllegoXDatAllSigs(file, timerange);
 
-%% fs is 30k, we will downsample to 1k
-
 % sanity check: 
-% timeSamples = downsample(signalStruct.timeSamples,30); % seconds
-stimIn      = downsample(signalStruct.signals(33,:),30); % microvolts
+% timeSamples = signalStruct.timeSamples; % seconds
+
+% stimulus-in channel: 
+stimIn = signalStruct.signals(33,:); % microvolts
+
+% data channels:
 if exist('channels','var')
     % take the channels input variable
-    data = downsample(signalStruct.signals(channels(1):channels(2),:)',30)';
+    data = signalStruct.signals(channels(1):channels(2),:);
 else % or
     % allow the user to change how many channels are analyzed
     prompt   = {'First Chan:','Last Chan:'};
@@ -22,7 +26,6 @@ else % or
     dims     = [1 35];
     definput = {'1','32'};
     channels = inputdlg(prompt,dlgtitle,dims,definput);
-    % downsample with that info it's 30k sr, I want 1k
-    data = downsample(signalStruct.signals(str2double(channels{1}):str2double(channels{2}),:)',30)';
+    data = signalStruct.signals(str2double(channels{1}):str2double(channels{2}),:);
 end
 
