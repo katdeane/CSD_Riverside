c24276abe43e2a95bf3b2ce044535acad65b57c3
katdeane <katdeane92@gmail.com>
2023-12-17 04:21:12 +0000

    adding group average visualization

--- Pipeline_CaliMouse.m
+++ Pipeline_CaliMouse.m
@@ -15,10 +15,10 @@ addpath(genpath(homedir));
 set(0, 'DefaultFigureRenderer', 'painters');
 
 % set consistently needed variables
-Groups = {'VMP'}; % 'PMP' % virgin male pupcall & paired male pupcall
-Condition = {'Pupcall'};
-% Condition = {'NoiseBurst' 'Pupcall' 'PostNoiseBurst'};
-cbar = [-0.8 0.8]; % species specific based on experience, color axis
+Groups = {'PMP' 'VMP'}; % virgin male pupcall & parent male pupcall
+% Condition = {'NoiseBurst'};
+Condition = {'NoiseBurst' 'Spontaneous' 'Pupcall30' 'PostNoiseBurst' 'ClickTrain' 'gapASSR' 'Chirp' 'Tonotopy'};
+cbar = [-0.5 0.5]; % species specific based on experience, color axis
 
 %% Data generation per subject ⊂◉‿◉つ
 
@@ -36,20 +36,46 @@ for iGro = 1:length(Groups)
     end
 end
 
+%% group pics
+
+% generate group averaged CSDs based on stimuli (does not BF sort)
+for iGro = 1:length(Groups)
+    for iST = 1:length(Condition)
+        disp(['Average CSDs for ' Groups{iGro} ' ' Condition{iST}])
+        tic
+        AvgCSDfig(homedir, Groups{iGro}, Condition{iST})
+        toc
+    end
+end
+
+
+% to do: some stackedgroup concatonation not working (spont and gapASSR 6
+% ms)
+
+disp('Producing group-averaged traces for each group')
+for iGro = 2:length(Groups)
+    for iST = 1:length(Condition)
+        disp(['Group traces for ' Groups{iGro} ' ' Condition{iST}])
+        tic 
+        Group_Avrec_Layers(homedir, Groups{iGro}, Condition{iST})
+        toc
+    end
+end
+
 %% Determine strength of response over EACH trial 
 
 % this is specifically to explore temporal dynamics over recording day and
 % uses single trial peak detection CSVs created by Avrec_Layers.m
-
-disp('Determining cortical strength over time')
-for iGro = 1:length(Groups)
-    for iST = 1:length(Condition)
-        disp(['For ' Groups{iGro} ' ' Condition{iST}])
-        tic 
-        StrengthxTime(homedir, Groups{iGro}, Condition{iST})
-        toc
-    end
-end
+% 
+% disp('Determining cortical strength over time')
+% for iGro = 1:length(Groups)
+%     for iST = 1:length(Condition)
+%         disp(['For ' Groups{iGro} ' ' Condition{iST}])
+%         tic 
+%         StrengthxTime(homedir, Groups{iGro}, Condition{iST})
+%         toc
+%     end
+% end
 
 %% CWT analysis 
 
@@ -60,9 +86,10 @@ params.voicesPerOctave = 8;
 params.timeBandWidth = 54;
 params.layers = {'II','IV','Va','Vb','VI'}; 
 params.condList = {'Pupcall'}; % subset %'NoiseBurst',
-params.groups = {'VMP','VMP'}; % for permutation test
+params.groups = {'PMP','VMP'}; % for permutation test
 
 % Only run when data regeneration is needed:
+runCwtCsd(homedir,'PMP',params);
 runCwtCsd(homedir,'VMP',params);
 
 % specifying Power: trials are averaged and then power is taken from
@@ -81,18 +108,28 @@ PermutationTest(homedir,'Phase',params,yespermute)
 delete(gcp('nocreate')) % end this par session
 
 %% Fast fourier transform of the spontaneous data 
-runFftCsd(homedir,params)
-plotFFT(homedir,params)
+% runFftCsd(homedir,params)
+% plotFFT(homedir,params)
 
 %% Interlaminar Phase Coherence
-LaminarPhaseLocking(homedir,params)
-interlamPhaseFig(homedir,params)
+% LaminarPhaseLocking(homedir,params)
+% interlamPhaseFig(homedir,params)
 
 %% Subject specific pup call visualization
-subject = 'VMP02';
+subject = 'PMP01';
 
 PupcallCSD(homedir,subject,cbar) % having run DynamicCSD
-PupcallTraces(homedir,subject)
+PupcallTraces(homedir,subject) % having run DynamicCSD
 PupcallCWT(homedir,subject,params) % having run runCwtCsd
+
+%% Average group pup call visualization
+
+GroupPupcallCSD(homedir,'VMP',cbar) % having run DynamicCSD
+GroupPupcallTraces(homedir,'VMP') % having run Avrec_Layers
+GroupPupcallCWT(homedir,'VMP',params) % having run runCwtCsd
+
+GroupPupcallCSD(homedir,'VMP',cbar) % having run DynamicCSD
+GroupPupcallTraces(homedir,'VMP') % having run Avrec_Layers
+GroupPupcallCWT(homedir,'VMP',params) % having run runCwtCsd
 
 
