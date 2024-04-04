bd87823d1fe63253479f6e84cc7bc6a2accdca3a
katdeane <katdeane92@gmail.com>
2024-04-04 20:20:54 +0000

    Added early and late timepoint to get clustermass

--- functions/ForCWT/dothepermute.m
+++ functions/ForCWT/dothepermute.m
@@ -1,5 +1,6 @@
-function [mass_clustermass, perm_layer] = dothepermute(whichtest,grp1Out,grp2Out,grp1size,grp2size,...
-    compTime,osciName,osciRows,t_thresh,nperms)
+function [mass_clustermass1,mass_clustermass2, perm_layer1,perm_layer2] = ...
+    dothepermute(whichtest,grp1Out,grp2Out,grp1size,grp2size,...
+    compTime1,compTime2,osciName,osciRows,t_thresh,nperms)
 
 if length(osciName) ~= 6
     warning('Your frequency bands dont match what they should. You should look into that.')
@@ -7,13 +8,21 @@ if length(osciName) ~= 6
 end
 
 % preallocate
-mass_clustermass        = NaN([1 nperms]);
-theta_clustermass       = NaN([1 nperms]);
-alpha_clustermass       = NaN([1 nperms]);
-betalow_clustermass     = NaN([1 nperms]);
-betahigh_clustermass    = NaN([1 nperms]);
-gammalow_clustermass    = NaN([1 nperms]);
-gammahigh_clustermass   = NaN([1 nperms]);
+mass_clustermass1        = NaN([1 nperms]);
+theta_clustermass1       = NaN([1 nperms]);
+alpha_clustermass1       = NaN([1 nperms]);
+betalow_clustermass1     = NaN([1 nperms]);
+betahigh_clustermass1    = NaN([1 nperms]);
+gammalow_clustermass1    = NaN([1 nperms]);
+gammahigh_clustermass1   = NaN([1 nperms]);
+
+mass_clustermass2        = NaN([1 nperms]);
+theta_clustermass2       = NaN([1 nperms]);
+alpha_clustermass2       = NaN([1 nperms]);
+betalow_clustermass2     = NaN([1 nperms]);
+betahigh_clustermass2    = NaN([1 nperms]);
+gammalow_clustermass2    = NaN([1 nperms]);
+gammahigh_clustermass2   = NaN([1 nperms]);
 
 % open the frequency bands back out (avoid overhead global struct use in
 % parloop)
@@ -23,6 +32,14 @@ beta_low   = osciRows{3};
 beta_high  = osciRows{4};
 gamma_low  = osciRows{5};
 gamma_high = osciRows{6};
+
+compTime2_1 = compTime2{1}; % all
+compTime2_2 = compTime2{2}; % theta
+compTime2_3 = compTime2{3}; % ...
+compTime2_4 = compTime2{4};
+compTime2_5 = compTime2{5};
+compTime2_6 = compTime2{6};
+compTime2_7 = compTime2{7};
 
 parfor iperm = 1:nperms
     % put the whole group in one container
@@ -51,21 +68,37 @@ parfor iperm = 1:nperms
     end
 
     % check cluster mass for compTime (BL:stimDur)
-    mass_clustermass(iperm)        = sum(sum(per_clusters(:,compTime)));
-    theta_clustermass(iperm)       = sum(sum(per_clusters(theta,compTime)));
-    alpha_clustermass(iperm)       = sum(sum(per_clusters(alpha,compTime)));
-    betalow_clustermass(iperm)     = sum(sum(per_clusters(beta_low,compTime)));
-    betahigh_clustermass(iperm)    = sum(sum(per_clusters(beta_high,compTime)));
-    gammalow_clustermass(iperm)    = sum(sum(per_clusters(gamma_low,compTime)));
-    gammahigh_clustermass(iperm)   = sum(sum(per_clusters(gamma_high,compTime)));
+    mass_clustermass1(iperm)        = sum(sum(per_clusters(:,compTime1)));
+    theta_clustermass1(iperm)       = sum(sum(per_clusters(theta,compTime1)));
+    alpha_clustermass1(iperm)       = sum(sum(per_clusters(alpha,compTime1)));
+    betalow_clustermass1(iperm)     = sum(sum(per_clusters(beta_low,compTime1)));
+    betahigh_clustermass1(iperm)    = sum(sum(per_clusters(beta_high,compTime1)));
+    gammalow_clustermass1(iperm)    = sum(sum(per_clusters(gamma_low,compTime1)));
+    gammahigh_clustermass1(iperm)   = sum(sum(per_clusters(gamma_high,compTime1)));
+
+    mass_clustermass2(iperm)        = sum(sum(per_clusters(:,compTime2_1)));
+    theta_clustermass2(iperm)       = sum(sum(per_clusters(theta,compTime2_2)));
+    alpha_clustermass2(iperm)       = sum(sum(per_clusters(alpha,compTime2_3)));
+    betalow_clustermass2(iperm)     = sum(sum(per_clusters(beta_low,compTime2_4)));
+    betahigh_clustermass2(iperm)    = sum(sum(per_clusters(beta_high,compTime2_5)));
+    gammalow_clustermass2(iperm)    = sum(sum(per_clusters(gamma_low,compTime2_6)));
+    gammahigh_clustermass2(iperm)   = sum(sum(per_clusters(gamma_high,compTime2_7)));
 
 end
 
 % make it an output struct
-perm_layer = struct;
-perm_layer.(osciName{1}) = theta_clustermass;
-perm_layer.(osciName{2}) = alpha_clustermass;
-perm_layer.(osciName{3}) = betalow_clustermass;
-perm_layer.(osciName{4}) = betahigh_clustermass;
-perm_layer.(osciName{5}) = gammalow_clustermass;
-perm_layer.(osciName{6}) = gammahigh_clustermass;
+perm_layer1 = struct;
+perm_layer1.(osciName{1}) = theta_clustermass1;
+perm_layer1.(osciName{2}) = alpha_clustermass1;
+perm_layer1.(osciName{3}) = betalow_clustermass1;
+perm_layer1.(osciName{4}) = betahigh_clustermass1;
+perm_layer1.(osciName{5}) = gammalow_clustermass1;
+perm_layer1.(osciName{6}) = gammahigh_clustermass1;
+
+perm_layer2 = struct;
+perm_layer2.(osciName{1}) = theta_clustermass2;
+perm_layer2.(osciName{2}) = alpha_clustermass2;
+perm_layer2.(osciName{3}) = betalow_clustermass2;
+perm_layer2.(osciName{4}) = betahigh_clustermass2;
+perm_layer2.(osciName{5}) = gammalow_clustermass2;
+perm_layer2.(osciName{6}) = gammahigh_clustermass2;
---
+++ functions/StimVariableCWT.m
@@ -0,0 +1,90 @@
+function [stimList, thisUnit, stimDur, stimITI, thisTag,compDur1,compDur2] = ...
+    StimVariableCWT(Condition,sr_mult)
+
+% for the various scripts that need this information dynamically, this
+% function reads the current stim type and produces consistant variable
+% data across the pipeline
+
+% sr_mult variable let's us control the sampling rate for CSD and Spike
+% output. CSD is consistently set at sr 1000 / sr_mult = 1. Spiking data
+% currently set to sr 3000 / sr_mult = 3.
+
+if ~exist('sr_mult','var')
+    sr_mult = 1; % 1k sampling rate
+end
+
+% compare at the onset
+compDur1 = 1:100; 
+
+if matches(Condition,'NoiseBurst') || ...
+        matches(Condition,'postNoise') || ...
+        matches(Condition, 'PostNoiseBurst')
+    stimList = [20 70];
+    thisUnit = 'dB';
+    stimDur  = 100*sr_mult; % ms
+    stimITI  = 1000*sr_mult;
+    thisTag  = 'noise'; 
+    % post processing
+    compDur2 = {100:300; 100:300; 100:300; 100:300; 100:300; 100:300; 100:300};
+    
+elseif matches(Condition,'Tonotopy')
+    stimList = [1, 2, 4, 8, 16, 24, 32];
+    thisUnit = 'kHz';
+    stimDur  = 200*sr_mult; % ms
+    stimITI  = 1000*sr_mult;
+    thisTag  = 'Tonotopy'; 
+    % post processing
+    compDur2 = {100:300; 100:300; 100:300; 100:300; 100:300; 100:300; 100:300};
+    
+elseif matches(Condition,'Spontaneous') || ...
+        matches(Condition,'postSpont')
+    stimList = 1;
+    thisUnit = [];
+    stimDur  = 1000*sr_mult; % ms
+    stimITI  = 1000*sr_mult;
+    thisTag  = 'spont'; 
+    compDur2 = 0; % probably don't need
+    
+elseif matches(Condition,'ClickTrain')
+    stimList = [1, 5, 10, 20, 40, 80];
+    thisUnit = 'Hz';
+    stimDur  = 2000*sr_mult; % ms
+    stimITI  = 1000*sr_mult; % processing 1 s but ITI actually 2s
+    thisTag  = 'ClickRate';
+    % ASSR reached
+    compDur2 = {1000:1500; 1000:1500; 1000:1500; ...
+        1000:1500; 1000:1500; 1000:1500; 1000:1500};
+    
+elseif matches(Condition,'Chirp')
+    stimList = 1;
+    thisUnit = [];
+    stimDur  = 3000*sr_mult; % ms
+    stimITI  = 1000*sr_mult; % processing 1 s but ITI actually 2s
+    thisTag  = 'single';
+    compDur2 = {1100:3000; []; []; 1100:1450; 1250:1750; ...
+        1500:2350; 1900:3000}; % post processing
+    
+elseif matches(Condition,'gapASSR')
+    % 10 gaps every 25 ms from onset to onset (40 hz)
+    % 250 ms noise, 250 ms gap-noise, etc. , 250 noise
+    % 10 presentations of gap-noise
+    % noiseonset = [0, 500, 1000, 1500, 2000, 2500, 3000, 3500, 4000, 4500, 5000];
+    % gaponset = [250, 750, 1250, 1750, 2250, 2750, 3250, 3750, 4250, 4750];
+    stimList = [2, 10];
+    thisUnit = ' [ms] gap width';
+    stimDur  = 3250*sr_mult; % ms
+    stimITI  = 500*sr_mult;
+    thisTag  = 'gapASSRRate';
+    % 4th gap in noise block
+    compDur2 = {1750:2000; 1750:2000; 1750:2000; ...
+        1750:2000; 1750:2000; 1750:2000; 1750:2000};
+
+elseif contains(Condition,'Pupcall') 
+    stimList = 1;
+    thisUnit = [];
+    stimDur  = 25027*sr_mult; % 25.027 s
+    stimITI  = 1000*sr_mult; % processing 1 s but ITI actually 5s
+    thisTag  = 'single';
+    compDur2 = 0; % DIDN'T CALCULATE
+
+end
