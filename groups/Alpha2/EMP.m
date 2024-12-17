7db444e458a2999a4a58869a84a1df4ace6080d3
ahuss031 <ahuss031@ucr.edu>
2024-12-17 12:05:23 -0800

    Updated Aging Data
    Took out FYN01 & FON01 from dataset, added new FYN13 & FON12 & FOS12 into dataset to replace. Dataset should be completed at this point.

--- groups/Alpha2/EMP.m
+++ groups/Alpha2/EMP.m
@@ -1,6 +1,6 @@
 %%EMP - Just an empty script for AJ to fill in single animals to look at
 %%CSD
-animals = {'AKO08'};  
+animals = {'FYN13'};  
  
 % notes:
 
@@ -9,23 +9,23 @@ animals = {'AKO08'};
 %channel order: [17 16 18 15 19 14 20 13 21 12 22 11 23 10 24 9 25 8 26 7 27 6 28 5 29 4 30 3 31 2 32 1]
 
 channels = {...
-  '[11 23 10 24 9 25 8 26 7 27 6 28 5 29 4 30 3 31 2 32 1]'
+  '[13 21 12 22 11 23 10 24 9 25 8 26 7 27 6 28 5 29 4 30]'
    };
 
 %             
-Layer.II = {'[]'}; 
+Layer.II = {'[1:3]'}; 
 %              
-Layer.IV = {'[]'};
+Layer.IV = {'[4:7]'};
 %              
-Layer.Va = {'[]'};
+Layer.Va = {'[8:11]'};
 %              
-Layer.Vb = {'[]'}; 
+Layer.Vb = {'[12:15]'}; 
 %              
-Layer.VI = {'[]'};
+Layer.VI = {'[16:20]'};
 
 %% Conditions
 Cond.NoiseBurst70 = {...
-    {'04'},... 
+    {'02'},... 
     
     };
 
